d8721ad1a649d3abe39638b44a804689c2740166
YeJin-Kim-code <kyjin5802@naver.com>
2022-07-29 01:47:48 +0900

    Add C File [PointerPractice1.c]

---
+++ PointerPractice1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 100, num2 = 100;
+	int* pnum;
+
+	pnum = &num1;
+	(*pnum) += 30;
+
+	pnum = &num2;
+	(*pnum) -= 30;
+
+	printf("%d, %d", num1, num2);
+	return 0;
+}
